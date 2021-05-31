155d4e2c59efd7cde9362b146e17306ec9292652
BuildTools <timpoknews@gmail.com>
2021-05-31 23:12:01 +0200

    Modif formulaire + réseau hyperlien new tab

--- target.php
+++ target.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>target</title>
-</head>
-<body>
-    <?php
-        echo "Bonjour".$_POST["prenom"];
-    ?>
-</body>
-</html>
+<?php
+
+
+    echo "Bonjour".$_POST["prenom"];
