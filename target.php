7e676b64f8daefad08f27ced4b2ec23942ec9707
BuildTools <timpoknews@gmail.com>
2021-06-01 20:32:47 +0200

    Avancement php sql

--- target.php
+++ target.php
@@ -1,5 +1,14 @@
 <?php
 
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=lionfit;charset=utf8', 'root', '');
+}
+
+catch(Exception $e) {
+    die('erreur :'.$e->getMessage());
+}
+$req = $bdd->prepare('INSERT INTO user(nom, prenom, age, genre, abonnement) VALUES(?, ?, ?, ?, ?)');
+$req->execute(array($_POST['nom'], $_POST['prenom'], $_POST['age'], $_POST['genre'], $_POST['abonnement']));
 
     echo "Bonjour".$_POST["prenom"];
 ?>
