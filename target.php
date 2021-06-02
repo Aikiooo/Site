d01d1cca7d4d8f718c4438bef8469b271c8bb5cb
BuildTools <timpoknews@gmail.com>
2021-06-02 11:32:55 +0200

    Php + lien abonnement

--- target.php
+++ target.php
@@ -7,8 +7,39 @@ try {
 catch(Exception $e) {
     die('erreur :'.$e->getMessage());
 }
-$req = $bdd->prepare('INSERT INTO user(nom, prenom, age, genre, abonnement) VALUES(?, ?, ?, ?, ?)');
+$req = $bdd->prepare('INSERT INTO users(nom, prenom, age, genre, abonnement) VALUES(?, ?, ?, ?, ?)');
 $req->execute(array($_POST['nom'], $_POST['prenom'], $_POST['age'], $_POST['genre'], $_POST['abonnement']));
 
-    echo "Bonjour".$_POST["prenom"];
+    echo "Bonjour ".$_POST["prenom"];
+    echo "Votre abonnement et vos données ont bien été enregistré, vous pouvez vous rendre à notre salle de sport dès maintenant.";
+    echo "<br>Vous allez être redirigé vers la page d'accueil dans 1 minute.";
+    sleep(5);
+    header('Location: homepage.html')
+    
 ?>
+ <!--
+
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=lionfit;charset=utf8', 'root', '');
+}
+
+catch(Exception $e) {
+    die('erreur :'.$e->getMessage());
+}
+
+
+
+if (strlen("nom") >= 3){
+    echo "Votre abonnement et vos données ont bien été enregistré, vous pouvez vous rendre à notre salle de sport dès maintenant.";
+    $req = $bdd->prepare('INSERT INTO users(nom, prenom, age, genre, abonnement) VALUES(?, ?, ?, ?, ?)');
+    $req->execute(array($_POST['nom'], $_POST['prenom'], $_POST['age'], $_POST['genre'], $_POST['abonnement']));
+    echo "<br> Vous allez à présent être redirigé sur la page principale";
+    sleep(60);
+    header('Location: homepage.html');
+
+
+} 
+else 
+    echo "Bonjour ".$_POST["prenom"];
+ -->
+
