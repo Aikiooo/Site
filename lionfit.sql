ecd796e16ef173f4c6a1c47d4cc0b574381bf2bc
BuildTools <timpoknews@gmail.com>
2021-06-02 23:39:17 +0200

    update sql

--- lionfit.sql
+++ lionfit.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : mar. 01 juin 2021 à 19:56
--- Version du serveur :  10.4.18-MariaDB
--- Version de PHP : 8.0.3
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mer. 02 juin 2021 à 21:36
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -27,13 +27,21 @@ SET time_zone = "+00:00";
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `nom` varchar(265) NOT NULL,
   `prenom` varchar(265) NOT NULL,
   `age` int(11) NOT NULL,
   `genre` int(11) NOT NULL,
   `abonnement` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`nom`, `prenom`, `age`, `genre`, `abonnement`) VALUES
+('Josh', 'Ethan', 20, 3, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
